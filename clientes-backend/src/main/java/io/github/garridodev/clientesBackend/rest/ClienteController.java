fa7d0fc118204bce315905218886148d729ab94d
Emanuel <emanuel.garrido@hotmail.com>
2021-05-25 21:16:30 -0300

    Buscando cliente por Id

--- clientes-backend/src/main/java/io/github/garridodev/clientesBackend/rest/ClienteController.java
+++ clientes-backend/src/main/java/io/github/garridodev/clientesBackend/rest/ClienteController.java
@@ -5,6 +5,7 @@ import io.github.garridodev.clientesBackend.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -22,4 +23,11 @@ public class ClienteController {
     public Cliente salvar( @RequestBody Cliente cliente ){
         return repository.save(cliente);
     };
+
+    @GetMapping("{id}")
+    public Cliente acharPorId( @PathVariable("id") Integer id){
+        return repository
+                .findById(id)
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND) );
+    }
 }
